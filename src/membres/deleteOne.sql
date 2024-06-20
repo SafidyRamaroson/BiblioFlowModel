ed5a95e9e2c36ff9b057b93dd9608a0de6d6eab8
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-21 01:53:49 +0300

    feat:sql for deleting on member

---
+++ src/membres/deleteOne.sql
@@ -0,0 +1,8 @@
+-- ####################################################################
+-- # Basic DELETE statement
+-- # See https://www.ibm.com/docs/en/db2-for-zos/13?topic=statements-delete for complete syntax.
+-- ####################################################################
+
+-- Delete One Membre
+DELETE FROM membre
+    WHERE id = "M06";
