603256ca875c52f45041cf8fa0f5354428a0ac65
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-21 02:08:48 +0300

    feat:sql for updating membre

---
+++ src/membres/updateOne.sql
@@ -0,0 +1,10 @@
+
+-- Update Member
+UPDATE membre
+    SET 
+    nom = "Malala",
+    email = "Malala@gmail.com",
+    sexe = "FEMININ",
+    age = 32,
+    contact = "+261 32 40 976 97" 
+    WHERE id = "M06";
